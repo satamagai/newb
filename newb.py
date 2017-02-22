d2816211bfbd93e12e089e268d97b3c45ede1d16
satamagai <satamagai@amagais-hqlm1.hhmi.org>
2017-02-22 12:34:32 -0500

    print out treasure

--- newb.py
+++ newb.py
@@ -1,6 +1,10 @@
 print ("testing")
 print ("testing")
 print ("Heeeelp")
+with open ('treasure.txt') as fh:
+  for line in fh:
+    print (line)
+
 for i in range(20):
 	print (str(i) + "bottles of rum")
 
